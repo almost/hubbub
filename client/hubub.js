09b33ea666cb056eafb1ce8956a0bed2a36f4b53
Thomas Parslow <tom@almostobsolete.net>
2014-11-18 15:12:20 +0000

    Removed unused code

--- client/hubub.js
+++ client/hubub.js
@@ -108,45 +108,6 @@
   document.addEventListener('submit', onSubmit);
   
 
-  function hubub (form, pendingComments) {
-    
-    var form = document.querySelector('form[data-hubbub]');
-    form.addEventListener('submit', function (evt) {
-      evt.preventDefault();
-      var comment = form.querySelector('[name=comment]').value;
-      var name = form.querySelector('[name=name]').value;
-      
-      var xmlhttp = new XMLHttpRequest();
-      // TODO: Make this configurable!
-      xmlhttp.open("POST","https://hubbub.herokuapp.com/api/default/comments",true);
-      xmlhttp.setRequestHeader("Content-type", "application/json");
-
-      xmlhttp.onload = function (e) {
-        if (xmlhttp.readyState === 4) {
-          if (xmlhttp.status >= 200 && xmlhttp.status < 300) {
-            var response = JSON.parse(xmlhttp.responseText);
-            var commentsContainer = document.querySelector('[data-hubbub-pendingcomment]');         
-            if (commentsContainer) {
-              var commentEl = document.createElement('div');
-              commentEl.innerHTML = response.html;
-              commentsContainer.appendChild(commentEl);
-            }
-            alert("Comment sent!");
-            form.querySelector('[name=comment]').value = "";
-          } else {
-            alert("Failed to send comment: " + xmlhttp.statusText);
-          }
-        }
-      };
-      xmlhttp.onerror = function (e) {
-        alert("Failed to send comment: " + xmlhttp.statusText);
-      };
-      
-      // TOOD: Get post path from canonical meta tag if it's present
-      xmlhttp.send(JSON.stringify({metadata: {name: name}, comment: comment, post: window.location.pathname}));
-    });
-  }
-  
   hubbub.defaults = defaults;
   hubbub.sendComment = sendComment;
   hubbub.sendForm = sendForm;
